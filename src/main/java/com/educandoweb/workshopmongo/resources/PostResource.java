3e989fc4ed7c49eda3efd83d1c614e0f9de06667
Andressa Pedrosa <ddressa.silva@gmail.com>
2021-02-16 12:42:36 -0300

    Testar Access Token

--- src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
@@ -31,6 +31,7 @@ public class PostResource {
 	
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+		
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		
